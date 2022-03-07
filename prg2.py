485348ce29968cf368d48206079a0153b539ec9b
Ardrapdas <94525953+Ardrapdas@users.noreply.github.com>
2022-03-07 12:34:12 +0530

    Add files via upload

---
+++ prg2.py
@@ -0,0 +1,19 @@
+list = []
+str = input("enter a string:")
+length = len(str)
+print("the string is", str)
+for i in range(0, length):
+    a = input()
+    list.append(a)
+print("the list is:", list)
+def upper():
+    print("enter the upper cases")
+def lower():
+    print("enter the lower cases")
+print("the number of lowercases and upper cases are:")
+
+
+
+
+
+
